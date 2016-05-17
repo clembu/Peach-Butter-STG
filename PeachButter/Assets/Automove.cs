e3df11039c6343b50e7d254b948f688320c8c988
facelesspanda <clement.busschaert@outlook.com>
2016-05-17 10:52:18 +0200

    SHOTS FIRED

---
+++ PeachButter/Assets/Automove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Automove : MonoBehaviour
+{
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+
+        rb2d.velocity = transform.up * speed;
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
--- PeachButter/Assets/PlayerController.cs
+++ PeachButter/Assets/PlayerController.cs
@@ -7,13 +7,35 @@ public class PlayerController : MonoBehaviour {
 
     Rigidbody2D rb2d;
 
+    public Transform shotSpawn1;
+
+    public Transform shotSpawn2;
+
+    public GameObject shot;
+
+    public float fireRate = 0.5f;
+
+    float nextFire = 0.0f;
+
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
 	}
 	
+    void Update()
+    {
+        bool input = Input.GetButton("Fire1");
+
+        if (input && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn1.position, shotSpawn1.rotation);
+            Instantiate(shot, shotSpawn2.position, shotSpawn2.rotation);
+        }
+    }
+
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
         Vector2 input = new Vector2( Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") );
 
