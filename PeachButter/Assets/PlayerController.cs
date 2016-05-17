8daac46c85e032724dbdc2695d85d2ee11886e13
facelesspanda <clement.busschaert@outlook.com>
2016-05-17 02:24:02 +0200

    Moving spaceship
    testing movement

---
+++ PeachButter/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 input = new Vector2( Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") );
+
+        Vector2 dir = input.normalized;
+
+        rb2d.velocity = dir * speed;        
+	}
+}
